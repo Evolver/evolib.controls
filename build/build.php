462acfa1a9268c20a1cf384fc85071dfbdf1ada5
Dmitry Stepanov <dmitrij@stepanov.lv>
2010-06-20 17:54:06 +0300

    Overall control extension update.

--- build/build.php
+++ build/build.php
@@ -28,17 +28,17 @@ NOTE;
 
 file_put_contents( __DIR__ .'/evolib.controls.js',
 	$authorNote .
-  lib_file( 'Control.js') ."\n\n" .
-  lib_file( 'BoolControl.js') ."\n\n" .
-  lib_file( 'ManualInputControl.js') ."\n\n" .
-  lib_file( 'Option.js') ."\n\n" .
-  lib_file( 'OptionControl.js') ."\n\n" .
-  lib_file( 'DropdownOption.js') ."\n\n" .
-  lib_file( 'DropdownOptionControl.js') ."\n\n" .
-  lib_file( 'StringControl.js') ."\n\n" .
-  lib_file( 'CheckboxControl.js') ."\n\n" .
-  lib_file( 'Form.js') ."\n\n" .
-  lib_file( 'common_API.js')
+  lib_file( 'controls.Control.js') ."\n\n" .
+  lib_file( 'controls.BoolControl.js') ."\n\n" .
+  lib_file( 'controls.ManualInputControl.js') ."\n\n" .
+  lib_file( 'controls.Option.js') ."\n\n" .
+  lib_file( 'controls.OptionControl.js') ."\n\n" .
+  lib_file( 'controls.DropdownOption.js') ."\n\n" .
+  lib_file( 'controls.DropdownOptionControl.js') ."\n\n" .
+  lib_file( 'controls.StringControl.js') ."\n\n" .
+  lib_file( 'controls.CheckboxControl.js') ."\n\n" .
+  lib_file( 'controls.Form.js') ."\n\n" .
+  lib_file( 'controls.js')
 );
 
 ?>
